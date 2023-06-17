53db4fc1297b3e8ca844b841fff7c33ad632ea40
Philip Rothschild <philiprene89@gmail.com>
2023-06-17 15:11:45 +0200

    ♻️ refactor(page.tsx): remove commented out code to improve code readability and maintainability

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -2,14 +2,7 @@ import { SignIn } from '@clerk/nextjs'
 
 const SignInPage = () => (
   <div className="flex h-[calc(100vh-theme(spacing.16))] items-center justify-center py-10">
-    <SignIn
-    // appearance={{
-    //   elements: {
-    //     formButtonPrimary:
-    //       'bg-slate-500 hover:bg-slate-400 text-sm normal-case'
-    //   }
-    // }}
-    />
+    <SignIn />
   </div>
 )
 
